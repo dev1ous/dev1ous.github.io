c22ef604b58a08bbb1f86108d0458a27536bc68a
dev1ous <60881111+dev1ous@users.noreply.github.com>
2025-01-12 20:30:26 +0100

    update

--- src/cours_test.cpp
+++ src/cours_test.cpp
@@ -6,25 +6,6 @@ int main()
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cute_image.jpg"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("arial.ttf"))
-        return EXIT_FAILURE;
-    sf::Text text("Hello SFML", font, 50);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("nice_music.ogg"))
-        return EXIT_FAILURE;
-
-    music.play();
-
     // Start the game loop
     while (window.isOpen())
     {
@@ -39,12 +20,6 @@ int main()
 
         // Clear screen
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
 
         // Update the window
         window.display();
